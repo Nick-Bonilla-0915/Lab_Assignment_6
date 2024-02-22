6f31065b8d143c6f31835c833f8c6fe9a074c04c
Nick-Bonilla-0915 <ni865929@ucf.edu>
2024-02-22 16:04:49 -0500

    Commit

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -2,6 +2,26 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
+	int mid, found = 0;
+
+	if(low <= high)
+	{
+		mid = (low + high)/2;
+		if(value < numbers[mid] && found == 0)
+		{
+			return search(numbers, low, mid-1, value);
+		}
+		else if(value > numbers[mid] && found == 0)
+		{
+			return search(numbers, mid + 1, high, value);
+		}
+		else if(value == numbers[mid] && found == 0)
+		{
+			found == 1;
+			return mid;
+		}
+	}
+
 	return -1;
 }
 
